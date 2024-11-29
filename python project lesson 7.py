2e412038f6525cd8f1121d990dc0a164075c7342
shayan2007 <Shayan.suhail@gmail.com>
2024-11-29 16:06:05 +0500

    h

--- python project lesson 7.py
+++ python project lesson 7.py
@@ -6,10 +6,10 @@ class A1:
         self.brain = brain
         self.insane = insane
     
-    def cntss(self, name):
+    def changename(self, name):
         self.name = name
     
-    def into(self):
+    def intro(self):
         print(f"hello i am {self.name} i am {self.age} years old")
         if self.brain:
             print("i have a brain")
@@ -19,7 +19,7 @@ class A1:
 thing1 = A1("siebel", -200, False, True)
 thing2 = A1("shayan", 17, True, True)
 
-thing1.cntss("Siebel")
-thing2.cntss("Shayan")
-thing1.into()
-thing2.into()
+thing1.changename("Siebel")
+thing2.changename("Shayan")
+thing1.intro()
+thing2.intro()
