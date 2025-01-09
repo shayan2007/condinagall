1ee308373096680f5ce414f05c997ed209ba24e8
shayan2007 <Shayan.suhail@gmail.com>
2025-01-09 21:20:05 +0500

    asd

--- python project lesson 15.py
+++ python project lesson 15.py
@@ -9,3 +9,13 @@ def deaf(n):
 
 print("995")
 print(deaf(995))
+
+def blind(n):
+    sum = 0
+    for i in range(n):
+        sum = sum + n
+        print(n)
+    return sum
+
+print("10000000")
+print(blind(10000000))
