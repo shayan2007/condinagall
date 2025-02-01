abbefca6da136a03334de64ebea5bd73a39e4873
shayan2007 <Shayan.suhail@gmail.com>
2025-02-01 13:13:24 +0500

    asd

--- poo.py
+++ poo.py
@@ -1,13 +1,18 @@
-list_of_numero = [1, 1, 2, 4, 4, 4, 4]
-thing = 0
-for i in range(len(list_of_numero)):
-    thing = list_of_numero[i]^thing
-print(thing)
+list_of_numero = [2, 2, 4, 6]
+xorof2 = list_of_numero[0]
+x = 0
+y = 0
+
+bit = 0
+for i in range(1, len(list_of_numero)):
+    xorof2 = xorof2 ^ list_of_numero[i]
+bit = xorof2 & ~(xorof2-1)
 
 for i in range(len(list_of_numero)):
-    occ = 0
-    for e in range(len(list_of_numero)):
-        if list_of_numero[i] == list_of_numero[e]:
-            occ += 1
-    if occ %2 == 1:
-        print(list_of_numero[i])
+    if list_of_numero[i] & bit:
+        x = x ^ list_of_numero[i]
+    else:
+        y= y ^ list_of_numero[i]
+
+print("x:", x)
+print("y:", y)
