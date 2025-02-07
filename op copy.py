446cec0c734a18425fe2a3bcc2999f0d1e7acda5
shayan2007 <Shayan.suhail@gmail.com>
2025-02-07 15:53:13 +0500

    wasd

---
+++ op copy.py
@@ -0,0 +1,17 @@
+import math
+n1 = 7
+n2 = 2000
+
+
+flop = 0
+while (n1 > 0 or n2 > 0):
+    t1 = (n1 & 1)
+    t2 = (n2 & 1)
+    if t1 != t2:
+        flop += 1
+
+    n1>>=1
+    n2>>=1
+
+print(flop)
+print(n1)
